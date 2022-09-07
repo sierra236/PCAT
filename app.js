48426230ec77b79a4c527011771b99a757319b2d
Dogan <dogaan.ds@gmail.com>
2022-09-07 17:36:37 +0300

    Add temp

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+  const photo = {
+    id: 1,
+    name: "Photo Name",
+    description: "Photo description "
+  }
+    res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is started on port ${port} `);
+});
