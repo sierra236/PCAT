b7637a6ec1f3662d185908f1cc6df0c819216de9
sierra236 <dogaan.ds@gmail.com>
2022-09-28 11:57:47 +0300

    connected DB and photo refinements

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const ejs= require('ejs')
+const mongoose = require('mongoose');
+const ejs= require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo')
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -20,7 +33,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
- 
+app.post('/photos', async (req, res) => {
+await Photo.create(req.body)
+res.redirect('/')
+}); 
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is started on port ${port} `);
